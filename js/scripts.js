0c6e20a34c4b97ab058e4e348c4961f16b7bc478
Caleb Paul and Francis Prus <caleb@>
2016-08-22 08:31:38 -0700

    Create html page with js functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,24 @@
 //Business Logic
-var textList = [];
-var ping3 = "ping";
-var pong5 = "pong";
-var pingPong15 = "ping-pong"
-
-function numList(number) {
-  textList.splice(0);
-  textList.push(0);
-  for (var i = 1; i <= number; i++) {
-    textList.push(i);
-    if (i % 15 === 0) {
-      textList.splice(i, 1, pingPong15)
-    } else if (i % 5 === 0) {
-      textList.splice(i, 1, pong5)
-    } else if (i % 3 === 0) {
-      textList.splice(i, 1, ping3)
-    }
-  }
-  return(textList.splice(0, 1));
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
 }
+
+
 //User Logic
 $(document).ready(function() {
-  $("#pong_form").submit(function(event){
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
-    var startNum = parseInt($("#num_input").val());
-    var output = numList(startNum)
 
-    function arrayToList(){
-      var nums = "";
-      for (var i = 0; i < textList.length; i++) {
-        nums += "<li>" + textList[i] + "</li>";
-      }
-      $("#pong_output").empty().append(nums);
-    }
-    arrayToList(textList);
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+
   });
 });
